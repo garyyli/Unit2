3ac1030a75dc95669f72410ac28b2630e65851c1
garyyli <garyli@hanovernorwichschools.org>
2017-09-13 13:45:41 -0400

    Updated from Brython Server: 9/13/2017 1:45:41 PM

--- gradeCalculator.py
+++ gradeCalculator.py
@@ -1,1 +1,14 @@
+#Gary Li
+#9/13/17
+#gradeCalculator.py - tells user what letter grade they got based off percentage
 
+grade = float(input('Input your grade: '))
+
+if grade>=90:
+    print('You earned an A')
+elif grade>=80:
+    print('You earned an B')
+elif grade>=70:
+    print('You earned an C')
+elif grade>=60:
+    print('You earned an D')
