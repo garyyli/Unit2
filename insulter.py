a1a0237a9db351e09c76754966ab73d2f65fad67
garyyli <garyli@hanovernorwichschools.org>
2017-09-14 14:51:34 -0400

    Updated from Brython Server: 9/14/2017 2:51:35 PM

--- insulter.py
+++ insulter.py
@@ -1,1 +1,23 @@
+#Gary Li
+#9/14/17
+#insulter.py - generates insults
 
+from random import randint
+number = randint(1,5)
+
+name = input('Enter your name: ')
+
+if number == 1:
+    print('You have no friends')
+
+if number == 2:
+    print('You literally have no friends')
+
+if number == 3:
+    print('You have ZERO friends')
+
+if number == 4:
+    print('You have no companions')
+
+if number == 5:
+    print('You actually have NADA AMIGOS')
