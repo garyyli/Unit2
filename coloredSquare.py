5bb1e7c726581f0527ac7e6a0ab7153b62e3ecc9
garyyli <garyli@hanovernorwichschools.org>
2017-09-18 14:45:50 -0400

    Updated from Brython Server: 9/18/2017 2:45:51 PM

--- coloredSquare.py
+++ coloredSquare.py
@@ -1,1 +1,13 @@
+#Gary Li
+#9/18/17
+#coloredSquare.py
 
+from ggame import *
+
+red = Color(0xff0000, 1)
+line = LineStyle(3,red)
+rectangle = RectangleAsset(100,100,line,red)
+
+Sprite(rectangle)
+myApp = App()
+myApp.run()
