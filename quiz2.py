d2bde7e6051f32d7fa49a3eae977f4d83aabd40d
garyyli <garyli@hanovernorwichschools.org>
2017-09-25 14:27:58 -0400

    Updated from Brython Server: 9/25/2017 2:27:58 PM

--- quiz2.py
+++ quiz2.py
@@ -1,1 +1,29 @@
+#Gary Li
+#9/25/17
+#quiz2.py
 
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter a 2nd number: '))
+
+if num1 == num2:
+    print('The numbers are the same')
+elif num1 > num2:
+    print('The first number is bigger')
+else:
+    print('The second number is bigger')
+
+if num1%3 == 0 and num2%3 == 0:
+    print('Both numbers are divisible by 3')
+elif num1%3 == 0:
+    print('Only the first number is divisible by 3')
+elif num2%3 == 0:
+    print('Only the second number is divisible by 3')
+else:
+    print('Neither number is divisible by 3')
+
+product = int(input('What is the product of your two numbers? '))
+
+if product == num1*num2:
+    print('Correct')
+else:
+    print('Incorrect')
