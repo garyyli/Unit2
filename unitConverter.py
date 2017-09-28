895becb501bc29b215b57b80557ff3f33b58b828
garyyli <garyli@hanovernorwichschools.org>
2017-09-28 14:45:28 -0400

    Updated from Brython Server: 9/28/2017 2:45:27 PM

--- unitConverter.py
+++ unitConverter.py
@@ -9,18 +9,19 @@ print('4) Celsius to Fahrenheit')
 
 number = float(input('Choose a number: '))
 
-if number==1:
-    kilometers = float(input('Enter number of kilometers: '))
-    print(kilometers, 'is', kilometers*0.621371, 'miles')
+for i in range (1,5):
+    if number==1:
+        kilometers = float(input('Enter number of kilometers: '))
+        print(kilometers, 'is', kilometers*0.621371, 'miles')
 
-if number==2:
-    kilograms = float(input('Enter number of kilograms: '))
-    print(kilograms, 'is', kilograms*2.20462, 'pounds')
+    if number==2:
+        kilograms = float(input('Enter number of kilograms: '))
+        print(kilograms, 'is', kilograms*2.20462, 'pounds')
 
-if number==3:
-    liters = float(input('Enter number of liters: '))
-    print(liters, 'is', liters*0.264172, 'gallons')
+    if number==3:
+        liters = float(input('Enter number of liters: '))
+        print(liters, 'is', liters*0.264172, 'gallons')
 
-if number==4:
-    celsius = float(input('Enter Degrees in Celsius: '))
-    print(celsius, 'is', (celsius*1.8)+32, 'degrees in Fahrenheit')
+    if number==4:
+        celsius = float(input('Enter Degrees in Celsius: '))
+        print(celsius, 'is', (celsius*1.8)+32, 'degrees in Fahrenheit')
